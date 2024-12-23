eb51877c8ed5fa3956cf297ec7a95d93c2a88309
Corey Applegate <cpapplefamily@gmail.com>
2024-12-23 06:59:42 -0600

    Refactoring and Organization

--- src/main.cpp
+++ src/main.cpp
@@ -11,6 +11,12 @@
 */
 #include <Arduino.h>
 #include <ETH.h>
+#include <ArduinoJson.h>
+#include <Adafruit_NeoPixel.h>
+#include "StartMatch.h"               // Include the StartMatch header
+#include "postStopStatus.h"           // Include the postStopStatus header
+#include "AllianceStatus.h"           // Include the AllianceStatus header
+#include "Field_stack_lightStatus.h"  // Include the Field_stack_lightStatus header
 
 #ifndef ETH_PHY_CS
 #define ETH_PHY_TYPE     ETH_PHY_W5500
@@ -24,6 +30,17 @@
 #define ETH_PHY_SPI_MOSI 11
 #endif
 
+//const char* baseUrl = "http://192.168.10.106:8080";
+const char* baseUrl = "http://10.0.100.5:8080";
+
+// Pins connected to the stop button
+#define NUM_BUTTONS 7
+const int stopButtonPins[NUM_BUTTONS] = {0, 1, 2, 3, 4, 5, 6};
+volatile bool stopButtonPressed[NUM_BUTTONS] = {false, false, false, false, false, false, false};
+
+#define START_MATCH_BTN 33
+#define LEDSTRIP 21           // Pin connected to NeoPixel
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(10, LEDSTRIP, NEO_GRB + NEO_KHZ800);
 
 static bool eth_connected = false;
 
@@ -32,7 +49,7 @@ void onEvent(arduino_event_id_t event, arduino_event_info_t info) {
     case ARDUINO_EVENT_ETH_START:
       Serial.println("ETH Started");
       //set eth hostname here
-      ETH.setHostname("esp32-eth0");
+      ETH.setHostname("Freezy_ScoreTable");
       break;
     case ARDUINO_EVENT_ETH_CONNECTED: 
       Serial.println("ETH Connected"); 
@@ -58,28 +75,7 @@ void onEvent(arduino_event_id_t event, arduino_event_info_t info) {
   }
 }
 
-#include <ArduinoJson.h>
-#include <Adafruit_NeoPixel.h>
-#include "StartMatch.h"               // Include the StartMatch header
-#include "postStopStatus.h"           // Include the postStopStatus header
-#include "AllianceStatus.h"           // Include the AllianceStatus header
-#include "Field_stack_lightStatus.h"  // Include the Field_stack_lightStatus header
-
-//const char* baseUrl = "http://192.168.10.106:8080";
-const char* baseUrl = "http://10.0.100.5:8080";
-
-// Pins connected to the stop button
-#define NUM_BUTTONS 7
-const int stopButtonPins[NUM_BUTTONS] = {0, 1, 2, 3, 4, 5, 6};
-volatile bool stopButtonPressed[NUM_BUTTONS] = {false, false, false, false, false, false, false};
-
-#define startMatchBTN 33
-
-#define led 18
-
-#define PIN 21           // Pin connected to NeoPixel
-Adafruit_NeoPixel strip = Adafruit_NeoPixel(10, PIN, NEO_GRB + NEO_KHZ800);
-
+// Interrupt handlers for the stop buttons
 void IRAM_ATTR handleStopButtonPress0() { stopButtonPressed[0] = true; }
 void IRAM_ATTR handleStopButtonPress1() { stopButtonPressed[1] = true; }
 void IRAM_ATTR handleStopButtonPress2() { stopButtonPressed[2] = true; }
@@ -88,7 +84,7 @@ void IRAM_ATTR handleStopButtonPress4() { stopButtonPressed[4] = true; }
 void IRAM_ATTR handleStopButtonPress5() { stopButtonPressed[5] = true; }
 void IRAM_ATTR handleStopButtonPress6() { stopButtonPressed[6] = true; }
 
-
+// Setup function
 void setup() {
   Serial.begin(115200);
   delay(1000);
@@ -99,7 +95,7 @@ void setup() {
   strip.show(); // Initialize all pixels to 'off'
 
   // Initialize the start match button
-  pinMode(startMatchBTN, INPUT_PULLUP);
+  pinMode(START_MATCH_BTN, INPUT_PULLUP);
 
    // Initialize the stop buttons
   for (int i = 0; i < NUM_BUTTONS; i++) {
@@ -113,20 +109,18 @@ void setup() {
   attachInterrupt(digitalPinToInterrupt(stopButtonPins[4]), handleStopButtonPress4, FALLING);
   attachInterrupt(digitalPinToInterrupt(stopButtonPins[5]), handleStopButtonPress5, FALLING);
   attachInterrupt(digitalPinToInterrupt(stopButtonPins[6]), handleStopButtonPress6, FALLING);
-  
-  // Initialize the LED's pin as an output
-  pinMode(led, OUTPUT);
 
   Network.onEvent(onEvent);
   ETH.begin(ETH_PHY_TYPE, ETH_PHY_ADDR, ETH_PHY_CS, ETH_PHY_IRQ, ETH_PHY_RST, ETH_PHY_SPI_HOST, ETH_PHY_SPI_SCK, ETH_PHY_SPI_MISO, ETH_PHY_SPI_MOSI);
 }
 
+// Main loop
 void loop() {
   static unsigned long lastStatusCheck = 0;
     unsigned long currentMillis = millis();
 
     // Check if the start match button is pressed
-    if (digitalRead(startMatchBTN) == LOW) {
+    if (digitalRead(START_MATCH_BTN) == LOW) {
         Serial.println("Start match button pressed!");
         startMatchPost();
     }
@@ -140,17 +134,11 @@ void loop() {
         } 
     }
     
-
     // Check alliance status every 500ms
     if (currentMillis - lastStatusCheck >= 500) {
         //getAllianceStatus();
         getField_stack_lightStatus();
-        
         lastStatusCheck = currentMillis;
     }
         strip.show();
-  /* if (eth_connected) {
-   
-  }
-  delay(500); */
 }
